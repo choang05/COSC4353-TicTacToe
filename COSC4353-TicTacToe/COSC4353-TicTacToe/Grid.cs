9918e8cbd2b08411c6cd96d5ca4a47fa7a34bcc6
Chad Hoang <chad.hoang@yahoo.com>
2017-03-21 14:57:21 -0500

    Initial project set up

---
+++ COSC4353-TicTacToe/COSC4353-TicTacToe/Grid.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Grid
+{
+    public static int GridSize;
+    public static GridPoint[,] GridPoints;
+
+    public struct GridPoint
+    {
+        public int xCoord;
+        public int yCoord;
+        public bool isOccupied;
+
+        public InputType input;
+        public enum InputType  {   X, O, NULL    };
+
+        public GridPoint(int xCoord, int yCoord, bool isOccupied, InputType input)
+        {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+            this.isOccupied = isOccupied;
+            this.input = input;
+        }
+    }
+
+    public Grid(int gridSize)
+    {
+        GridSize = gridSize;
+        GenerateGrid(GridSize);
+    }
+
+    //  Generate the grid points with default values
+    public void GenerateGrid(int gridSize)
+    {
+        GridPoints = new GridPoint[gridSize, gridSize];
+
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                GridPoints[x, y] = new GridPoint(x, y, false, GridPoint.InputType.NULL);
+            }
+        }
+    }
+}
---
+++ COSC4353-TicTacToe/COSC4353-TicTacToe/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    //  User-defined board size
+    private const int BoardSize = 1;
+
+    static void Main(string[] args)
+    {
+        //  Initialize grid
+        Grid grid = new Grid(BoardSize);
+
+        //  Prompt user for grid size
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        Console.WriteLine();    //  Skip a line
+
+        Console.Write("Please enter a difficulty number from 1-3: ");
+
+        string difficultyInput = Console.ReadLine();
+
+        //  Evaluate input for validity, if not, repeat request
+        while (!IsDifficultyInputValid(difficultyInput))
+        {
+            Console.WriteLine("Invalid input!");
+            Console.WriteLine();    //  Skip a line
+            Console.WriteLine("Please enter a difficulty number from 1-3:");
+
+            difficultyInput = Console.ReadLine();
+        }
+
+        int difficultyLevel = int.Parse(difficultyInput);
+
+        Console.WriteLine("Difficulty level: " + difficultyLevel);
+
+        Console.WriteLine();    //  Skip a line
+
+        //  Pause console so it doesn't close
+        Console.ReadLine();
+    }
+
+    //  Returns true if given string is a integer. Otherwise, return false
+    private static bool IsDifficultyInputValid(string input)
+    {
+        // determine if input is a integer 
+        int difficultyLevel;
+        bool isNumeric = int.TryParse(input, out difficultyLevel);
+        if (isNumeric)
+        {
+            //  Determine if input is within valid range
+            if (difficultyLevel > 0 && difficultyLevel <= 3)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+}
